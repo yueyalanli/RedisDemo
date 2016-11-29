4418c245f1b5186988a43e6d2b803b4afda03db2
lenovo <qingjulfe@163.com>
2016-11-29 15:20:46 +0800

    adding

---
+++ src/main/java/com/redis/student/impl/TeacherInfoServiceImpl.java
@@ -0,0 +1,30 @@
+package com.redis.student.impl;
+
+import com.redis.teacher.TeacherInfoService;
+
+import java.util.List;
+
+/**
+ * Created by lenovo on 2016/11/29.
+ */
+public class TeacherInfoServiceImpl implements TeacherInfoService {
+    @Override
+    public boolean insertTeacherInfo(List<String> teacherID) {
+        return false;
+    }
+
+    @Override
+    public boolean delTeacherInfo(List<String> teacherID) {
+        return false;
+    }
+
+    @Override
+    public boolean editTeacherInfo(String teacherID) {
+        return false;
+    }
+
+    @Override
+    public List<String> selectTeacherInfo(List<String> teacherID) {
+        return null;
+    }
+}
--- src/main/java/com/redis/teacher/TeacherInfoService.java
+++ src/main/java/com/redis/teacher/TeacherInfoService.java
@@ -1,7 +1,18 @@
 package com.redis.teacher;
 
+import java.util.List;
+
 /**
  * Created by lenovo on 2016/11/29.
  */
-public class TeacherInfoService {
+public interface TeacherInfoService {
+    //批量增加老师信息
+    boolean insertTeacherInfo(List<String> teacherID);
+    //批量删除
+    boolean delTeacherInfo(List<String> teacherID);
+    //修改
+    boolean editTeacherInfo(String teacherID);
+    //查询
+    List<String> selectTeacherInfo(List<String> teacherID);
+
 }
