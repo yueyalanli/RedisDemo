79e4d22e811860ae7ca1e7a3a2f45975a4d13c00
lenovo <qingjulfe@163.com>
2016-11-29 16:29:57 +0800

    adding

--- src/main/java/com/redis/utils/RedisUtils.java
+++ src/main/java/com/redis/utils/RedisUtils.java
@@ -1,7 +1,30 @@
 package com.redis.utils;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import redis.clients.jedis.ShardedJedis;
+import redis.clients.jedis.ShardedJedisPool;
+
 /**
  * Created by lenovo on 2016/11/29.
  */
 public class RedisUtils {
+    @Autowired
+    public ShardedJedisPool shardedJedisPool;
+
+    public ShardedJedis getShardedJeids(){
+        ShardedJedis jds = null;
+        try{
+            jds = shardedJedisPool.getResource();
+            return jds;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return jds;
+    }
+
+    public void returnResource(ShardedJedis shardedJedis){
+        if(shardedJedis != null){
+            shardedJedisPool.returnResource(shardedJedis);
+        }
+    }
 }
